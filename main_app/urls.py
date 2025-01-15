5f31b82b482cf22feb2d0f33e946580e150fe910
Tony Gadaleta <anthonygadaleta@gmail.com>
2025-01-15 11:32:19 -0500

    one to many

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import Home, CatList, CatDetail
+from .views import Home, CatList, CatDetail, FeedingListCreate, FeedingDetail
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
     path('cats/', CatList.as_view(), name='cat-list'),
     path('cats/<int:id>/', CatDetail.as_view(), name='cat-detail'),
+    path('cats/<int:cat_id>/feedings/', FeedingListCreate.as_view(), name='feeding-list-create'),
+    path('cats/<int:cat_id>/feedings/<int:id>/', FeedingDetail.as_view(), name='feeding-detail'),
 ]
